c2f1a76a1a6dfe7085d10b45a4dfa1df9e1413e9
carlfeynman <coolarun477@gmail.com>
2024-07-04 11:11:00 +0530

    server initialization done

--- r_main.py
+++ r_main.py
@@ -1,9 +1,19 @@
 import sqlite3
 from contextlib import contextmanager
+from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import docker
+import logging
 
 DB_NAME = 'problems_v9.db'
 PROBLEM_TABLE = 'problems'
 SUBMISSION_TEST_CASE_TABLE = 'submission_test_cases'
+
+class DB:
+    def __init__(self, cursor, conn):
+        self.cursor = cursor
+        self.conn = conn
 
 @contextmanager
 def create_sql_connection():
@@ -11,7 +21,29 @@ def create_sql_connection():
     try:
         conn = sqlite3.connect(DB_NAME)
         conn.row_factory = sqlite3.Row
-        yield conn
+        yield DB(cursor=conn.cursor(), conn=conn)
     finally:
         if conn:
             conn.close()
+
+def get_problem(problem_id: str):
+    with create_sql_connection() as _db:
+        return _db.cursor.execute(
+            f"SELECT * FROM {PROBLEM_TABLE} WHERE questionId = ?", (problem_id, )
+        ).fetchone()  
+
+def get_all_problems():
+    with create_sql_connection() as _db:
+        return _db.cursor.execute(
+            f"SELECT * FROM {PROBLEM_TABLE}"
+        ).fetchall()
+
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+app.mount('/assets', StaticFiles(directory='assets'), name='assets')
+docker_client = docker.from_env()
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
