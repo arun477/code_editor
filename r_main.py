a4c7b0e6b3fbbf6fa70d1342d4dc303ef97601e8
carlfeynman <coolarun477@gmail.com>
2024-07-04 12:16:58 +0530

    run docker function refactored

--- r_main.py
+++ r_main.py
@@ -5,9 +5,13 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
-import docker
-import logging
 from pydantic import BaseModel
+import tempfile
+import os
+import json
+import docker 
+import logging
+import shutil
 
 DB_NAME = 'problems_v9.db'
 PROBLEM_TABLE = 'problems'
@@ -41,7 +45,6 @@ def get_all_problems():
             f"SELECT * FROM {PROBLEM_TABLE}"
         ).fetchall()
 
-
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 app.mount('/assets', StaticFiles(directory='assets'), name='assets')
@@ -65,6 +68,106 @@ async def problem(problem_id: str):
         raise HTTPException(status_code=404, detail='invalid problem id')
     return dict(problem)
 
+def create_runnable_scripts(code, problem):
+    exec_script = templates.get_template('execution_script_v2.jinja2').render(validation_func=problem)
+    solution_script = templates.get_template('user_code_template.jinja2').render(user_code=code)
+    return exec_script, solution_script
+
+def temp_docker_mounting_folder(exec_script, solution_script, test_cases):
+    temp_dir = tempfile.mkdtemp()
+    with open(os.path.join(temp_dir, 'execution_script.py'), 'w') as dest:
+        dest.write(exec_script)
+    with open(os.path.join(temp_dir, 'solution.py'), 'w') as dest:
+        dest.write(solution_script)
+    with open(os.path.join(temp_dir, 'test_cases.json'), 'w') as dest:
+        dest.write(json.dumps(json.loads(test_cases)))
+    with open(os.path.join(temp_dir, '__init__.py'), 'w') as dest:
+        dest.write('')
+    return temp_dir
+
+@contextmanager
+def get_new_docker_container(config):
+    client = docker.from_env()
+    container = None
+    try:
+        container = client.containers.run(**config)
+    finally:
+        if container:
+            container.remove(force=True)
+
+class DockerConfig:
+    def __init__(self, volume_dir):
+        self.DOCKER_CONFIG = {
+            'image': 'python:3.9-slim',
+            'command': ['python', '-m', 'py_compile', '/app-compile/solution.py'],
+            'volumes': {
+                volume_dir : {'bind': '/app-compile', 'mode':'rw'},
+            },
+            'detach': True,
+            'mem_limit': '250m',
+            'cpu_quota': 50000,
+            'network_mode': 'none',
+            'read_only': True,
+            'user':'nobody',
+            'pids_limit': 50,
+            'security_opt': ['no-new-privileges']
+        }
+        return self.DOCKER_CONFIG
+    
+def run_code_validation(temp_dir):
+    docker_config = DockerConfig(volume_dir=temp_dir)
+    try:
+        with get_new_docker_container(docker_config) as container:
+            exit_code = container.wait(timeout=10)['StatusCode']
+            if exit_code != 0:
+                logs = container.logs(stdout=True, stderr=True).decode('utf-8')
+                return {'outputs': {}, 'error': str(logs)}
+    except OSError:
+        return { 'outputs': {}, 'error': "permission denied" }
+    except Exception as _:
+        return {'outputs': {}, 'error': 'an unexpected error occured'}
+    return None
+
+def run_user_solution_code(temp_dir):
+    docker_config = DockerConfig(volume_dir=temp_dir)
+    try:
+        with get_new_docker_container(docker_config) as container:
+            try:
+                container.wait(timeout=30)
+            except Exception as _:
+                return {'outputs': {}, 'error': "time limit exceeded" }
+            logs = container.logs(stdout=True, stderr=True).decode('utf-8')
+            if logs and logs.strip() == 'Killed':
+                return  { 'outputs': {}, 'error': "memory limit exceeded" }
+    
+            output_file_path = os.path.join(temp_dir, 'results', 'results.json')
+            with open(output_file_path, 'r') as file:
+                output = json.loads(file.read())
+            return {'outputs': output, 'logs': '', "error": output.get('error', None)}
+    except OSError:
+        return { 'outputs': {}, 'error': "permission denied" }
+    except Exception as _:
+        return {'outputs': {}, 'error': 'an unexpected error occured'}
+
+def remove_temp_dir(temp_dir):
+    try:
+        shutil.rmtree(temp_dir)
+    except Exception as _:
+        pass 
+
+def run_in_docker(code, problem):
+    exec_script, solution_script = create_runnable_scripts(code, problem)
+    temp_dir = temp_docker_mounting_folder(exec_script, solution_script, problem['test_cases'])
+
+    failed_validation = run_code_validation(temp_dir)
+    if failed_validation:
+        return failed_validation
+
+    execution_output = run_user_solution_code(temp_dir)
+    remove_temp_dir(temp_dir)
+    
+    return execution_output
+
 class RunCodeInput(BaseModel):
     problem_id: str
     code: str
@@ -72,6 +175,9 @@ class RunCodeInput(BaseModel):
 @app.post("/run_code")
 async def run_code(run_code_input:RunCodeInput):
     problem_id, code = run_code_input.problem_id, run_code_input.code
-    result = run_in_docker(code, problem_id)
+    problem = get_problem(problem_id)
+    if not problem:
+        raise HTTPException(status_code=404, detail='invalid problem id')
+    result = run_in_docker(code, problem)
     return result or {}
 
