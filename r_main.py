1fc1ce633d36f08294cb522b3e2dbc9d19f8b476
carlfeynman <coolarun477@gmail.com>
2024-07-04 11:21:16 +0530

    basic user routes are added

--- r_main.py
+++ r_main.py
@@ -1,10 +1,13 @@
 import sqlite3
 from contextlib import contextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.requests import Request
 import docker
 import logging
+from pydantic import BaseModel
 
 DB_NAME = 'problems_v9.db'
 PROBLEM_TABLE = 'problems'
@@ -38,6 +41,7 @@ def get_all_problems():
             f"SELECT * FROM {PROBLEM_TABLE}"
         ).fetchall()
 
+
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 app.mount('/assets', StaticFiles(directory='assets'), name='assets')
@@ -46,4 +50,28 @@ docker_client = docker.from_env()
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+@app.get('/', response_class=HTMLResponse)
+async def root(request:Request):
+    return templates.TemplateResponse("index.html", {'request': request})
+
+@app.get("/problem_page/{problem_id}", response_class=HTMLResponse)
+async def problem_page(request:Request):
+    return templates.TemplateResponse('problem.html', {'request': request})
+
+@app.get("/problem/{problem_id}")
+async def problem(problem_id: str):
+    problem = get_problem(problem_id)
+    if not problem:
+        raise HTTPException(status_code=404, detail='invalid problem id')
+    return dict(problem)
+
+class RunCodeInput(BaseModel):
+    problem_id: str
+    code: str
+
+@app.post("/run_code")
+async def run_code(run_code_input:RunCodeInput):
+    problem_id, code = run_code_input.problem_id, run_code_input.code
+    result = run_in_docker(code, problem_id)
+    return result or {}
 
