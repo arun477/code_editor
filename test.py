36ce3be28ab1e2f144b49677d4a923af0972d76b
carlfeynman <coolarun477@gmail.com>
2024-07-05 12:42:46 +0530

    multiprocessing added for concurrency

--- test.py
+++ test.py
@@ -1,25 +1,29 @@
+import requests
+import multiprocessing
 import time
-import aiohttp
-import asyncio
 
-async def download_site(url, session):
-    async with session.get(url) as res:
+session = None
+
+def set_global_session():
+    global session
+    if not session:
+        session = requests.Session()
+
+def download_site(url):
+    with session.get(url) as res:
         print(f"content: {len(res.content)} from {url}")
-        
-async def download_all_sites(sites):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for url in sites:
-            task = asyncio.ensure_future(download_site(url, session))
-            tasks.append(task)
-        await asyncio.gather(*tasks,return_exceptions=True)
-  
+
+def download_all_sites(sites):
+    with multiprocessing.Pool(initializer=set_global_session) as pool:
+        pool.map(download_site, sites)
+
+      
 if __name__ == '__main__':
     sites = [
          "https://www.jython.org",
         "http://olympus.realpython.org/dice",
     ] * 100
     start = time.time()
-    asyncio.get_event_loop().run_until_complete(download_all_sites(sites))
+    download_all_sites(sites)
     duration = time.time() - start
     print(f'downloaded {len(sites)} in {duration} secs')
