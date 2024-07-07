c38a352b043a871fe108ed09492295edfd260ec9
carlfeynman <coolarun477@gmail.com>
2024-07-07 10:40:20 +0530

    tested redis-queue option

--- test2.py
+++ test2.py
@@ -1,7 +1,9 @@
 import time
 def simple_task(x, y):
+    print('----------*---------')
     print(f"starting task: add {x} + {y}")
     time.sleep(10)
     result = x + y
     print(f"task completed {x} + {y} = {result}")
+    print('----------*---------')
     return result
